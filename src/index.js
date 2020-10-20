4a999332b152e9566aad1090d6e57ade2a214e16
ashabahmed <ashabahmed7@gmail.com>
2020-10-20 15:29:07 -0400

    weighttrackerdiv and form with patch request for user and renderUserWeightDiv with submithandler done

--- src/index.js
+++ src/index.js
@@ -1,29 +1,90 @@
 document.addEventListener("DOMContentLoaded", function() {
-const baseUrl = "http://localhost:3000/exercises/"
-  
-const getExercises = () => {
-    fetch(baseUrl)
-    .then(response => response.json())
-    .then(exercises => {
-        for(const exercise of exercises) {
-            renderExercise(exercise)
-        }
-    })
-}
+	const exUrl = "http://localhost:3000/exercises/"
+	const userUrl = "http://localhost:3000/users/4"
 
-const renderExercise = exercise => {
-  const exDiv = document.createElement("div")
-  exDiv.classList.add("card")
+		
+	// const getExercises = () => {
+	// 	fetch(exUrl)
+	// 	.then(response => response.json())
+	// 	.then(exercises => {
+	// 		renderExercises(exercises)
+	// 	})
+	// }
 
-  exDiv.innerHTML = `
-  <h1>${exercise.exercise}</h1>
-  <br>
-  <img src="${exercise.example}">
-  `
+	// const renderExercises = exercises => {
+	// 	for(const exercise of exercises) {
+	// 		renderExercise(exercise)
+	// 	}
+	// }
 
-  mainDiv = document.querySelector(".mainDiv")
-  mainDiv.appendChild(exDiv)
-}
+	// const renderExercise = exercise => {
+	// 	const exDiv = document.createElement("div")
+	// 	exDiv.classList.add("card")
 
-getExercises();
+	// 	exDiv.innerHTML = `
+	// 	<h1>${exercise.exercise}</h1>
+	// 	<br>
+	// 	<img src="${exercise.example}">
+	// 	`
+
+	// 	mainDiv = document.querySelector(".mainDiv")
+	// 	mainDiv.appendChild(exDiv)
+	// }
+
+	const getUser = () => {
+		fetch(userUrl)
+		.then(response => response.json())
+		.then(user => renderUserWeightDiv(user))
+	}
+
+	const renderUserWeightDiv = (user) => {
+		const stats = document.querySelector(".weightStats")
+		stats.innerHTML = `
+		<p>Goal Weight: ${user.goalWeight} lbs.</p>
+		<p>Current Weight: ${user.currentWeight} lbs.</p>
+		`
+
+		const weightUl = document.querySelector(".weightUl")
+		const newLi = document.createElement("li")
+		let d = new Date()
+		let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "Oct.", "November", "December"]
+		newLi.innerHTML = `${months[d.getMonth()]} ${d.getDate()}`
+
+		weightUl.appendChild(newLi)
+	}
+
+	const submitHandler = () => {
+		document.addEventListener('submit', e => {
+			e.preventDefault()
+			
+			const weightForm = document.getElementById('weightInput')
+			const currentWeight = weightForm.currentweight.value
+			
+
+			const weightInfo = {"currentWeight": currentWeight}
+
+			weightForm.reset()
+
+			const options = {
+				method: "PATCH",
+				headers: {
+					"content-type": "application/json",
+					"accept": "application/json"
+				},
+				body: JSON.stringify(weightInfo)
+			}
+
+			fetch(userUrl, options)
+			.then(response => response.json())
+			.then(getUser())
+		})
+	}
+
+	const clickHandler = () => {
+
+	}
+
+	getUser();
+	submitHandler();
+	// getExercises();
 })
