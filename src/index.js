f60fbebfdb9f747a06235d3af8ae2e900651144c
ashabahmed <ashabahmed7@gmail.com>
2020-10-20 23:34:06 -0400

    modified weight tracker and calorie tracker is working fine

--- src/index.js
+++ src/index.js
@@ -41,41 +41,40 @@ document.addEventListener("DOMContentLoaded", function() {
 		const stats = document.querySelector(".weightStats")
 		stats.innerHTML = `
 		<p>Goal Weight: ${user.goalWeight} lbs.</p>
+		<p>Starting Weight: ${user.startingWeight} lbs.</p>
 		<p>Current Weight: ${user.currentWeight.slice(-1)} lbs.</p>
 		<p>Pounds Lost: ${user.startingWeight - parseInt(user.currentWeight.slice(-1))} lbs.</p>
 		<p>Pounds Left: ${user.goalWeight - parseInt(user.currentWeight.slice(-1))} lbs.</p>
 		`
-		const weightUl = document.querySelector(".weightUl")
-		const newLi = document.createElement("li")
-		let d = new Date()
-		let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "Oct.", "November", "December"]
-		newLi.innerHTML = `${months[d.getMonth()]} ${d.getDate()} ${user.currentWeight[0]}`
+		// const weightUl = document.querySelector(".weightUl")
+		// const newLi = document.createElement("li")
+		// let d = new Date()
+		// let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "Oct.", "November", "December"]
+		// newLi.innerHTML = `
+		// <b>${months[d.getMonth()]} ${d.getDate()}:</b> ${user.currentWeight.slice(-1)}lbs
+		// `
 
-		weightUl.appendChild(newLi)
+		// weightUl.append(newLi)
 	}
 
 	const submitHandler = () => {
 		document.addEventListener('submit', e => {
 			e.preventDefault()
 			if(e.target.matches("#weightInput")) {
-				console.log(e.target)
 				const weightForm = document.getElementById('weightInput')
 				const currentWeight = weightForm.currentweight.value
-				const weightInfo = {currentWeight: currentWeight}
 				weightForm.reset()
 
-				const options = {
-					method: "POST",
-					headers: {
-						"content-type": "application/json",
-						"accept": "application/json"
-					},
-					body: JSON.stringify(weightInfo)
-				}
-
-				fetch(userUrl, options)
+				fetch(userUrl)
 				.then(response => response.json())
-				.then(getUser())
+				.then(data => {
+					const currentWeightArray = data.currentWeight
+					currentWeightArray.push(currentWeight)
+					console.log(currentWeightArray)
+					addCurrentWeight(currentWeightArray)
+					
+				})
+
 			} else if(e.target.matches("#caloriesInput")) {
 			
 				const macrosForm = document.getElementById('caloriesInput')
@@ -89,8 +88,53 @@ document.addEventListener("DOMContentLoaded", function() {
 		})
 	}
 
+	const addCurrentWeight = (currentWeightArray) => {
+		const options = {
+					method: "PATCH",
+					headers: {
+						"content-type": "application/json",
+						"accept": "application/json"
+					},
+					body: JSON.stringify({
+						currentWeight: currentWeightArray
+					})
+				}
+
+				fetch(userUrl, options)
+				.then(response => response.json())
+				.then(user => {
+					addCurrentWeightLi(user),
+					getUser()
+				})
+
+
+				
+	}
+
+	const addCurrentWeightLi = (user) => {
+		const weightUl = document.querySelector(".weightUl")
+		const newLi = document.createElement("li")
+		let d = new Date()
+		let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "Oct.", "November", "December"]
+		newLi.innerHTML = `
+		<b>${months[d.getMonth()]} ${d.getDate()}:</b> ${user.currentWeight.slice(-1)} lbs.
+		`
+
+		weightUl.append(newLi)
+	}
+
 	const updateMacros = (proteinInput, carbsInput, fatsInput) => {
-		console.log(proteinInput)
+		protein = parseInt(document.querySelector(".protein").innerText)
+		proteinInputInt = parseInt(proteinInput)
+		carbs = parseInt(document.querySelector(".carbs").innerText)
+		carbsInputInt = parseInt(carbsInput)
+		fats = parseInt(document.querySelector(".fats").innerText)
+		fatsInputInt = parseInt(fatsInput)
+
+		document.querySelector(".protein").innerHTML = `${protein - proteinInputInt}g Protein`
+		document.querySelector(".carbs").innerHTML = `${carbs - carbsInputInt}g Carbs`
+		document.querySelector(".fats").innerHTML = `${fats - fatsInputInt}g Fats`
+		
 	}
 
 	// const clickHandler = () => {
