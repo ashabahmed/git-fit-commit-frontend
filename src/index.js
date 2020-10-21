06bcf6501020f0e3bda54bfe12c863c03ee66441
mushfichowdhury <chowdhurymushfi@gmail.com>
2020-10-21 15:40:33 -0400

    added workout tracker

--- src/index.js
+++ src/index.js
@@ -1,22 +1,73 @@
 document.addEventListener("DOMContentLoaded", function() {
 	const exUrl = "http://localhost:3000/exercises/"
-	const userUrl = "http://localhost:3000/users/22"
+	const userUrl = "http://localhost:3000/users/23"
 
 		
-	// const getExercises = () => {
-	// 	fetch(exUrl)
-	// 	.then(response => response.json())
-	// 	.then(exercises => {
-	// 		renderExercises(exercises)
-	// 	})
-	// }
+	const getExercises = () => {
+		fetch(userUrl)
+		.then(response => response.json())
+		.then(user => {
+			renderUserExercises(user)
+		})
+	}
 
-	// const renderExercises = exercises => {
-	// 	for(const exercise of exercises) {
-	// 		renderExercise(exercise)
-	// 	}
-	// }
+	// Here are different ways we can do this workout tracker
+	/*
+		dynamically select exercises for each different user based on their equpiment attribute will select 
+		the equipment select will return a colleciton of worksout that use that equipment 
+						Also filter tthrough diffculity level no matter the equipment?
+		collection will have a lot of exercises that can be shuffled through the week,
+		each card will show the work outs for that day only 
+		different days will select random exercises for that day from collection 	
+		we can do this by doing an if statement to figure out the day			
 
+		each exercise will list number of sets and reps for each exercise
+		some exercise sets will be time based and not rep based
+
+		on exercise click we can render that specific exercise and all it's details(gif, description, insturction, notes)
+		we can get the id of the exercise and set it to the dataset id number for the p tag of the exercise
+
+	*/
+
+
+	const random_exercises = items => {
+		return items[Math.floor(Math.random()*items.length)]
+	}
+
+	const renderUserExercises = user => {
+		const userExercises = user.exercises
+		const fullBody = userExercises.filter(exercise => exercise.majorMuscle.includes("Full Body"))
+		const core = userExercises.filter(exercise => exercise.majorMuscle.includes("Core"))
+		const legs = userExercises.filter(exercise => exercise.majorMuscle.includes("Legs"))
+		const arms = userExercises.filter(exercise => exercise.majorMuscle.includes("Arms"))
+
+
+		const workoutDiv = document.querySelector("#workoutDiv")
+		const workout = document.createElement("div")
+		const fullBodysample1 = random_exercises(fullBody)
+		const armsSample = random_exercises(arms)
+		const legsSample = random_exercises(legs)
+		const coreSample = random_exercises(core)
+		const fullBodysample2 = random_exercises(fullBody)
+
+		
+		workoutDiv.innerHTML = `
+		<h2 class="card-title">Workout Tracker:</h2>
+		<p class="><b>${fullBodysample1.exercise}</b>: 3 Sets - 6-10 Reps each </p>
+		<p><b>${coreSample.exercise}</b>: 3 Sets - 6-10 Reps each </p>
+		<p><b>${legsSample.exercise}</b>: 2 Sets - 6-10 Reps each </p>
+		<p><b>${armsSample.exercise}</b>: 2 Sets - 10-12 Reps each </p>
+		<p><b>${fullBodysample2.exercise}</b>: 1 Set until failure </p>
+		`
+		workoutDiv.appendChild(workout)
+
+		
+		// for(const exercise of exercises) {
+		// 	renderExercise(exercise)
+		// }
+	}
+
+	
 	// const renderExercise = exercise => {
 	// 	const exDiv = document.createElement("div")
 	// 	exDiv.classList.add("card")
@@ -45,21 +96,6 @@ document.addEventListener("DOMContentLoaded", function() {
 		<p>Current Weight: ${user.currentWeight.slice(-1)} lbs.</p>
 		<p>Pounds Lost: ${user.startingWeight - parseInt(user.currentWeight.slice(-1))} lbs.</p>
 		<p>Pounds Left: ${user.goalWeight - parseInt(user.currentWeight.slice(-1))} lbs.</p>
-		`
-		// const weightUl = document.querySelector(".weightUl")
-		// const newLi = document.createElement("li")
-		// let d = new Date()
-		// let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "Oct.", "November", "December"]
-		// newLi.innerHTML = `
-		// <b>${months[d.getMonth()]} ${d.getDate()}:</b> ${user.currentWeight.slice(-1)}lbs
-		// `
-
-		// weightUl.append(newLi)
-
-		const nav = document.querySelector("#navBar")
-		nav.innerHTML = `
-		<h1 class="container p-5 my-3 bg-dark text-white"> Git Fit. Git Commit. </h1>
-		<h1 class="container p-5 my-3 bg-dark text-white">Hi ${user.name}</h1>
 		`
 	}
 
@@ -112,9 +148,6 @@ document.addEventListener("DOMContentLoaded", function() {
 					addCurrentWeightLi(user),
 					getUser()
 				})
-
-
-				
 	}
 
 	const addCurrentWeightLi = (user) => {
@@ -127,6 +160,23 @@ document.addEventListener("DOMContentLoaded", function() {
 		`
 
 		weightUl.append(newLi)
+		
+		renderProgressCard(user)
+	}
+
+	const renderProgressCard = (user) => {
+		const progressDiv = document.querySelector("#progressDiv")
+		const newProgressCard = document.createElement("div")
+		newProgressCard.innerHTML = `
+			<div class="card" style="width: 18rem; margin-left: 50px;">
+				<img class="card-img-top" src="https://img.etimg.com/photo/msid-74747053,quality-100/for-miles-a-great-bodyweight-workout-would-include-squats-push-ups-walking-lunges-.jpg" alt="Card image cap">
+				<div class="card-body">
+					<h5 class="card-title">Card title</h5>
+					<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+				</div>
+			</div>
+		`
+		progressDiv.appendChild(newProgressCard)
 	}
 
 	const updateMacros = (proteinInput, carbsInput, fatsInput) => {
@@ -143,11 +193,19 @@ document.addEventListener("DOMContentLoaded", function() {
 		
 	}
 
-	// const clickHandler = () => {
 
-	// }
+		// <!-- Workout Card is going provide exercises and workout regime for the week.  -->
+		// Make a function that renders all the user's exercises based off equipment.
+		// Format those exercises into a workout Program
+
+
+
+	// <!-- Make initial form for welcome page, so user can enter details -->
+	// <!-- based on that we can provide specified workouts -->
+	// <!-- Make sure all the stuff that's entered as form input saves to user's DB -->
+	// <!-- Name of exercise will be clickable and lead to that workout's details, gif, and instructions -->
 
 	getUser();
 	submitHandler();
-	// getExercises();
+	getExercises();
 })
