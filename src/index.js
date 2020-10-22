fa507d8581694309949429f1cebcfc272efaa240
mushfichowdhury <chowdhurymushfi@gmail.com>
2020-10-21 23:37:05 -0400

    commit

--- src/index.js
+++ src/index.js
@@ -15,61 +15,154 @@ document.addEventListener("DOMContentLoaded", function() {
 	weekday[5] = "Friday";
 	weekday[6] = "Saturday";
 
-	let n = weekday[d.getDay()];
+	let dayNum = 0;
+	let n = weekday[d.getDay() + dayNum];
+	let tomorrow = weekday[d.getDay() + 1]
+	let threeDaysFromNow = weekday[d.getDay() + 3]
 	
 	let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "Oct.", "November", "December"]
 
 	const exerciseInfoDiv = document.getElementById('exerciseInfo')
 	
+	function addDarkmodeWidget() {
+		new Darkmode().showWidget();
+	}
+	window.addEventListener('load', addDarkmodeWidget);
+
 	const getExercises = () => {
 		fetch(userUrl)
 		.then(response => response.json())
 		.then(user => {
+			
 			renderUserExercises(user)
 		})
 	}
-
 
 	const random_exercises = items => {
 		return items[Math.floor(Math.random()*items.length)]
 	}
 
 	const renderUserExercises = user => {
+		workoutDiv.innerHTML = ""
 		if(n === "Wednesday"){
+			
 			const fullBody = user.exercises.filter(exercise => exercise.majorMuscle.includes("Full Body"))
 			const core = user.exercises.filter(exercise => exercise.majorMuscle.includes("Core"))
 			const legs = user.exercises.filter(exercise => exercise.majorMuscle.includes("Legs"))
 			const arms = user.exercises.filter(exercise => exercise.majorMuscle.includes("Arms"))
 	
-	
 			const workoutDiv = document.querySelector("#workoutDiv")
 			const workout = document.createElement("div")
-			const fullBodysample1 = random_exercises(fullBody)
+			const fullBodySample1 = random_exercises(fullBody)
 			const armsSample = random_exercises(arms)
 			const legsSample = random_exercises(legs)
 			const coreSample = random_exercises(core)
-			const fullBodysample2 = random_exercises(fullBody)
+			const fullBodySample2 = random_exercises(fullBody)
+		
+			workoutDiv.innerHTML = `
+			<h2 class="card-title">Workout Tracker:</h2>
+			<h3 style="text-align:center;">${n}</h3>
+			<h4 style="text-align:center;"> <b> Rest for 3 minutes after each set!</b> </h4>
+			<br>
+			<button type="button" data-toggle="modal" data-target="#exerciseModal" style="background: none; border: none" class="workout-p" data-exercise-id="${fullBodySample1.id}"><b class="workout-p" data-exercise-id="${fullBodySample1.id}">${fullBodySample1.exercise}</b>: 3 Sets - 6-10 Reps each </button> <input type="checkbox" id="accept"> <span class="badge badge-pill badge-info">Done?</span>
+			<br>
+			<button type="button" data-toggle="modal" data-target="#exerciseModal" style="background: none; border: none" class="workout-p" data-exercise-id="${coreSample.id}"><b class="workout-p" data-exercise-id="${coreSample.id}">${coreSample.exercise}</b>: 3 Sets - 6-10 Reps each </button> <input type="checkbox" id="accept"> <span class="badge badge-pill badge-info">Done?</span>
+			<br>
+			<button type="button" data-toggle="modal" data-target="#exerciseModal" style="background: none; border: none" class="workout-p" data-exercise-id="${legsSample.id}"><b class="workout-p" data-exercise-id="${legsSample.id}">${legsSample.exercise}</b>: 2 Sets - 6-10 Reps each </button> <input type="checkbox" id="accept"> <span class="badge badge-pill badge-info">Done?</span>
+			<br>
+			<button type="button" data-toggle="modal" data-target="#exerciseModal" style="background: none; border: none" class="workout-p" data-exercise-id="${armsSample.id}"><b class="workout-p" data-exercise-id="${armsSample.id}">${armsSample.exercise}</b>: 2 Sets - 10-12 Reps each </button> <input type="checkbox" id="accept"> <span class="badge badge-pill badge-info">Done?</span>
+			<br>
+			<button type="button" data-toggle="modal" data-target="#exerciseModal" style="background: none; border: none" class="workout-p" data-exercise-id="${fullBodySample2.id}"><b class="workout-p" data-exercise-id="${fullBodySample2.id}">${fullBodySample2.exercise}</b>: 1 Set until failure </button> <input type="checkbox" id="accept"> <span class="badge badge-pill badge-info">Done?</span>
+			<br>
+			<ul class="pagination pagination-sm justify-content-end" style="margin:20px 0">
+				<li class="page-item" ><a id="prev-btn" class="page-link" href="#">Previous</a></li>
+			
+				<li class="page-item" ><a id="next-btn" class="page-link" href="#">Next</a></li>
+			</ul>
+
+			
+			`
+			workoutDiv.appendChild(workout)
+		} else if (n === "Friday"){
+			
+			const fullBody = user.exercises.filter(exercise => exercise.majorMuscle.includes("Full Body"))
+			const core = user.exercises.filter(exercise => exercise.majorMuscle.includes("Core"))
+			const legs = user.exercises.filter(exercise => exercise.majorMuscle.includes("Legs"))
+			const arms = user.exercises.filter(exercise => exercise.majorMuscle.includes("Arms"))
+			const back = user.exercises.filter(exercise => exercise.majorMuscle.includes("Back"))
+	
+			const workoutDiv = document.querySelector("#workoutDiv")
+			const workout = document.createElement("div")
+			const fullBodySample1 = random_exercises(fullBody)
+			const backSample = random_exercises(back)
+			const legsSample = random_exercises(legs)
+			const armsSample = random_exercises(arms)
+			const coreSample = random_exercises(core)
+			
+			workoutDiv.innerHTML = `
+			<h2 class="card-title">Workout Tracker:</h2>
+			<h3 style="text-align:center;">${n}</h3>
+			<h4 style="text-align:center;"> <b>Rest for 1 minute after each set!</b> </h4>
+			<br>
+			<p class="workout-p" data-exercise-id="${fullBodySample1.id}"><b class="workout-p" data-exercise-id="${fullBodySample1.id}">${fullBodySample1.exercise}</b>: 3 Sets - 6-10 Reps each </p> <input type="checkbox" id="accept"> <span class="badge badge-pill badge-info">Done?</span>
+			<p class="workout-p" data-exercise-id="${coreSample.id}"><b class="workout-p" data-exercise-id="${coreSample.id}">${coreSample.exercise}</b>: 3 Sets - 6-10 Reps each </p> <input type="checkbox" id="accept"> <span class="badge badge-pill badge-info">Done?</span>
+			<p class="workout-p" data-exercise-id="${backSample.id}"><b class="workout-p" data-exercise-id="${backSample.id}">${backSample.exercise}</b>: 2 Sets - 6-10 Reps each </p> <input type="checkbox" id="accept"> <span class="badge badge-pill badge-info">Done?</span>
+			<p class="workout-p" data-exercise-id="${armsSample.id}"><b class="workout-p" data-exercise-id="${armsSample.id}">${armsSample.exercise}</b>: 2 Sets - 10-12 Reps each </p> <input type="checkbox" id="accept"> <span class="badge badge-pill badge-info">Done?</span>
+			<p class="workout-p" data-exercise-id="${legsSample.id}"><b class="workout-p" data-exercise-id="${legsSample.id}">${legsSample.exercise}</b>: 1 Set until failure </p> <input type="checkbox" id="accept"> <span class="badge badge-pill badge-info">Done?</span>
+			<br>
+			<ul class="pagination pagination-sm justify-content-end" style="margin:20px 0">
+				<li class="page-item" ><a id="prev-btn" class="page-link" href="#">Previous</a></li>
+			
+				<li class="page-item" ><a id="next-btn" class="page-link" href="#">Next</a></li>
+			</ul>
+			`
+			workoutDiv.appendChild(workout)
+		} else if (n === "Monday") {
+			const fullBody = user.exercises.filter(exercise => exercise.majorMuscle.includes("Full Body"))
+			const core = user.exercises.filter(exercise => exercise.majorMuscle.includes("Core"))
+			const legs = user.exercises.filter(exercise => exercise.majorMuscle.includes("Legs"))
+			
+			const back = user.exercises.filter(exercise => exercise.majorMuscle.includes("Back"))
+	
+			const workoutDiv = document.querySelector("#workoutDiv")
+			const workout = document.createElement("div")
+			const fullBodySample1 = random_exercises(fullBody)
+			
+			const legsSample = random_exercises(legs)
+			const coreSample = random_exercises(core)
+			const legsSample2 = random_exercises(legs)
 	
 			
 			workoutDiv.innerHTML = `
 			<h2 class="card-title">Workout Tracker:</h2>
 			<h3 style="text-align:center;">${n}</h3>
+			<h4 style="text-align:center;"> <b>Rest for 2 minutes after each set!</b> </h4>
 			<br>
-			<p class="workout-p" data-exercise-id="${fullBodysample1.id}"><b class="workout-p" data-exercise-id="${fullBodysample1.id}">${fullBodysample1.exercise}</b>: 3 Sets - 6-10 Reps each </p> <input type="checkbox" id="accept"> Done.
-			<p class="workout-p" data-exercise-id="${coreSample.id}"><b class="workout-p" data-exercise-id="${coreSample.id}">${coreSample.exercise}</b>: 3 Sets - 6-10 Reps each </p> <input type="checkbox" id="accept"> Done.
-			<p class="workout-p" data-exercise-id="${legsSample.id}"><b class="workout-p" data-exercise-id="${legsSample.id}">${legsSample.exercise}</b>: 2 Sets - 6-10 Reps each </p> <input type="checkbox" id="accept"> Done.
-			<p class="workout-p" data-exercise-id="${armsSample.id}"><b class="workout-p" data-exercise-id="${armsSample.id}">${armsSample.exercise}</b>: 2 Sets - 10-12 Reps each </p> <input type="checkbox" id="accept"> Done.
-			<p class="workout-p" data-exercise-id="${fullBodysample2.id}"><b class="workout-p" data-exercise-id="${fullBodysample2.id}">${fullBodysample2.exercise}</b>: 1 Set until failure </p> <input type="checkbox" id="accept"> Done.
+			<p class="workout-p" data-exercise-id="${legsSample.id}"><b class="workout-p" data-exercise-id="${legsSample.id}">${legsSample.exercise}</b>: 3 Sets - 6-10 Reps each </p> <input type="checkbox" id="accept"> <span class="badge badge-pill badge-info">Done?</span>
+			<p class="workout-p" data-exercise-id="${coreSample.id}"><b class="workout-p" data-exercise-id="${coreSample.id}">${coreSample.exercise}</b>: 3 Sets - 6-10 Reps each </p> <input type="checkbox" id="accept"> <span class="badge badge-pill badge-info">Done?</span>
+			<p class="workout-p" data-exercise-id="${back.id}"><b class="workout-p" data-exercise-id="${back.id}">${back.exercise}</b>: 2 Sets - 6-10 Reps each </p> <input type="checkbox" id="accept"> <span class="badge badge-pill badge-info">Done?</span>
+			<p class="workout-p" data-exercise-id="${fullBodySample1.id}"><b class="workout-p" data-exercise-id="${fullBodySample1.id}">${fullBodySample1.exercise}</b>: 2 Sets - 10-12 Reps each </p> <input type="checkbox" id="accept"> <span class="badge badge-pill badge-info">Done?</span>
+			<p class="workout-p" data-exercise-id="${legsSample2.id}"><b class="workout-p" data-exercise-id="${legsSample2.id}">${legsSample2.exercise}</b>: 1 Set until failure </p> <input type="checkbox" id="accept"> <span class="badge badge-pill badge-info">Done?</span>
 			<br>
-			<ul class="pagination pagination-sm">
-			<li class="page-item"><a class="page-link" href="#">Previous</a></li>
-	
-			<li class="page-item"><a class="page-link" href="#">Next</a></li>
-		</ul>
+			<ul class="pagination pagination-sm justify-content-end" style="margin:20px 0">
+				<li class="page-item" ><a id="prev-btn" class="page-link" href="#">Previous</a></li>
+			
+				<li class="page-item" ><a id="next-btn" class="page-link" href="#">Next</a></li>
+			</ul>
 			`
 			workoutDiv.appendChild(workout)
 		} else {
-			console.log("REST DAY!!")
+			const workoutDiv = document.querySelector("#workoutDiv")
+			workoutDiv.innerHTML = `
+			<h1> REST DAY!! </h1>
+			<h3> Try for some light cardio if you're up for it! </h3>
+			<h3> Resting up is just dandy though, you're doing great!</h3>
+			<ul class="pagination pagination-sm justify-content-end" style="margin:20px 0">
+				<li class="page-item" ><a id="prev-btn" class="page-link" href="#">Previous</a></li>
+		
+				<li class="page-item" ><a id="next-btn" class="page-link" href="#">Next</a></li>
+			</ul>
+			`
 		}
 
 
@@ -79,7 +172,7 @@ document.addEventListener("DOMContentLoaded", function() {
 		// }
 	}
 
-	
+
 	// const renderExercise = exercise => {
 	// 	const exDiv = document.createElement("div")
 	// 	exDiv.classList.add("card")
@@ -138,9 +231,9 @@ document.addEventListener("DOMContentLoaded", function() {
 
 				updateMacros(proteinInput, carbsInput, fatsInput)
 			} else if(e.target.matches("#mainForm")) {
+				e.preventDefault();
 				
 				
-				console.log(e.target)
 			}
 		})
 	}
@@ -209,42 +302,73 @@ document.addEventListener("DOMContentLoaded", function() {
 
 	const clickHandler = () => {
 		document.addEventListener('click', e =>{
-			if(e.target.matches(".workout-p")){
-				const workout = e.target
-				const exerciseId = workout.dataset.exerciseId
-				
-				fetch(exUrl + exerciseId)
-				.then(response => response.json())
-				.then(exercise => {
-					renderExerciseInfo(exercise)
-				})
-			}
+			if (e.target.matches("#next-btn")) {
+					dayNum += 1
+					
+				} else if (e.target.matches("#prev-btn")) {
+					dayNum -= 1
+					
+				}
+
 		})
 
 	}
 
-	const renderExerciseInfo = exercise => {
-		exerciseInfoDiv.classList.add("card")
-		exerciseInfoDiv.innerHTML = `
-		<img src="${exercise.example}">
-		<p>${exercise.exercise}</p><br>
-		<p>${exercise.exerciseType}</p><br>
-		<p>${exercise.equipment}</p><br>
-		<p>${exercise.majorMuscle}</p><br>
-		<p>${exercise.minorMuscle}</p><br>
-		<p>${exercise.notes}</p><br>
-		`
+	// if(e.target.matches(".workout-p")){
+	// 	const workout = e.target
+	// 	const exerciseId = workout.dataset.exerciseId
+	// 	console.log(exerciseId)
+		
+	// 	fetch(exUrl + exerciseId)
+	// 	.then(response => response.json())
+	// 	.then(exercise => {
+	// 		renderExerciseInfo(exercise)
+	// 	})
+	// } else 
+	// const renderExerciseInfo = exercise => {
+	// 	exerciseInfoDiv.innerHTML = `
+	// 		<div class="modal-fade" id="exerciseModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+	// 			<div class="modal-dialog modal-dialog-centered" role="document">
+	// 				<div class="modal-content">
+	// 					<div class="modal-header">
+	// 						<h5 class="modal-title" id="exampleModalLongTitle">${exercise.exercise}</h5>
+	// 						<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+	// 							<span aria-hidden="true">&times;</span>
+	// 						</button>
+	// 					</div>
+	// 					<div class="modal-body">
+	// 					<img src="${exercise.example}">
+	// 					<p>${exercise.exerciseType}</p>
+	// 					<p>${exercise.equipment}</p>
+	// 					<p>${exercise.majorMuscle}</p>
+	// 					<p>${exercise.minorMuscle}</p>
+	// 					<p>${exercise.notes}</p>
+	// 					<p>${exercise.modifications}</p>
+	// 					</div>
+	// 				<div class="modal-footer">
+	// 				<button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+	// 				</div>
+	// 		</div>
+	// 		`
+	// }
 
+	$("#navbar_register_btn").on("click",function(e){
+		e.preventDefault();
+		$('#navModal').modal('show');
+	})
 
-	}
-
-
+	$(".workout-p").on("click",function(e){
+		e.preventDefault();
+		$('#exerciseModal').modal('show');
+	})
 
 	clickHandler();
 	getUser();
 	submitHandler();
 	getExercises();
 })
+
+
 
 
 
